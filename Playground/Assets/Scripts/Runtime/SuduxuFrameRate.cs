80626a285c243e27cc93aaa26eff206452fe9fed
lucaCase <luca.reisenbichler@gmx.at>
2025-12-28 12:02:17 +0100

    Added Set Frame Rate

---
+++ Playground/Assets/Scripts/Runtime/SuduxuFrameRate.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuduxuFrameRate : MonoBehaviour
+{
+    private Suduxu suduxu = new(1);
+
+    private int frameCount = 0;
+    private float elapsed = 0;
+
+    private void Start()
+    {
+        suduxu.Launch();
+        suduxu.RegisterCallbacks();
+
+        suduxu.Log.OnLog += (log) =>
+        {
+            Debug.Log(log);
+        };
+
+        suduxu.Input.OnSensorData += (ref SensorDataRaw sensorData) =>
+        {
+            Debug.Log(sensorData);
+
+            frameCount++;
+        };
+    }
+
+    private void Update()
+    {
+        suduxu.Input.Tick(Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            suduxu.Client.SetFrameRate(5);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            suduxu.Client.SendSensorData(true);
+        }
+
+        if (elapsed > 1)
+        {
+            Debug.Log($"Rounds: {frameCount}");
+
+            elapsed = 0;
+            frameCount = 0;
+        }
+
+        elapsed += Time.deltaTime;
+    }
+
+    private void OnDestroy()
+    {
+        suduxu.Stop();
+    }
+}
